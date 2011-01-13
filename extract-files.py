67bb3c00b67c9c42d293a354efbad7ad12bcfe9c
Nanhumly <admin@warbler.cc>
2011-01-13 08:12:07 +0000

    sagit: Switch to py extract utils
    Change-Id: Ia13b728464495ff208ca1a706847b11af5c93ae8

---
+++ extract-files.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_17_2'
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+namespace_imports = [
+    'vendor/xiaomi/msm8998-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/sensors/hals.conf': blob_fixup()
+        .regex_replace('sensors.elliptic.so\n', ''),
+    'vendor/lib64/libgf_hal.so': blob_fixup()
+        .remove_needed('libpowermanager.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sagit',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'msm8998-common', module.vendor
+    )
+    utils.run()
